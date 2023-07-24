f38f8a95353169f83a289e146b0e63b57cc19f24
raajveerk <khattarraajveer@gmail.com>
2023-07-24 15:23:36 +0530

    updated certain styling

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
       </div>
       <div className='relative wrapperForParallax flex justify-center'>
         {/* <div className='absolute w-[1550px] m-0 h-[3600px] top-[0px] bg-cover bg-scroll boop-bg'></div> */}
-        <div className='absolute w-[650px] h-[450px] top-[-100px] right-[-700px] md:top-[50px] md:right-[-535px] lg:top-[-420px] lg:right-[-635px] opacity-60 bg-contain bg-blend-soft-light bg-scroll bg-no-repeat blobUno'></div>
+        <div className='absolute w-[650px] h-[450px] top-[-100px] right-[-700px] md:top-[50px] md:right-[-535px] lg:top-[-220px] lg:right-[-835px] opacity-60 bg-contain bg-blend-soft-light bg-scroll bg-no-repeat blobUno'></div>
         {/* <div className='absolute w-[450px] h-[450px] top-[3750px] right-[-135px] bg-contain bg-scroll bg-no-repeat line'></div> */}
-        <div className='absolute w-[250px] h-[250px] top-[-150px] left-[-200px] lg:top-[-100px] lg:left-[-435px] bg-cover bg-scroll bg-no-repeat moon'></div>
-        <div className='absolute w-[250px] h-[250px] top-[850px] right-[-125px] md:top-[1050px] md:right-[-145px] lg:top-[925px] lg:right-[-245px] bg-cover bg-scroll bg-no-repeat rocket'></div>
-        <div className='absolute w-[250px] h-[250px] top-[1250px] left-[-125px] md:top-[1450px] md:left-[-185px] lg:top-[1550px] lg:left-[-255px] bg-cover bg-scroll bg-no-repeat spaceman'></div>
-        <div className='absolute top-[1850px] right-[-250px] md:top-[2165px] md:left-[50px] lg:top-[1665px] lg:left-[165px] bg-gradient-to-br from-grad-uno via-grad-dos to-grad-tres w-[550px] h-[450px] blobBackground blur-3xl opacity-70'></div>
-        <div className='absolute w-[250px] h-[250px] top-[2350px] right-[-110px] md:top-[2670px] md:right-[-125px] lg:top-[2225px] lg:right-[-160px] bg-cover bg-scroll bg-no-repeat tele'></div>
+        <div className='absolute w-[250px] h-[250px] top-[-150px] left-[-200px] lg:top-[30px] lg:left-[-395px] bg-cover bg-scroll bg-no-repeat moon'></div>
+        <div className='absolute w-[250px] h-[250px] top-[850px] right-[-125px] md:top-[1050px] md:right-[-145px] lg:top-[925px] lg:right-[-165px] bg-cover bg-scroll bg-no-repeat rocket'></div>
+        <div className='absolute w-[250px] h-[250px] top-[1250px] left-[-125px] md:top-[1450px] md:left-[-185px] lg:top-[1520px] lg:left-[-225px] bg-cover bg-scroll bg-no-repeat spaceman'></div>
+        <div className='absolute top-[1850px] right-[-250px] md:top-[2165px] md:left-[50px] lg:top-[1865px] lg:left-[380px] bg-gradient-to-br from-grad-uno via-grad-dos to-grad-tres w-[550px] h-[450px] blobBackground blur-3xl opacity-70'></div>
+        <div className='absolute w-[250px] h-[250px] top-[2350px] right-[-110px] md:top-[2670px] md:right-[-125px] lg:top-[2365px] lg:right-[-160px] bg-cover bg-scroll bg-no-repeat tele'></div>
         <div className='absolute w-[250px] h-[250px] top-[3350px] left-[-200px] md:top-[3750px] md:left-[-225px] lg:top-[3250px] lg:left-[-475px] bg-cover bg-scroll bg-no-repeat planet'></div>
         <div className='foreground'>
           <Hero />
